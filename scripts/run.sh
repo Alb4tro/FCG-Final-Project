aaffa68aade92158a837bc492cefb131327456a4
albatrosss <albertomongardini@gmail.com>
2020-05-24 16:54:17 +0200

    new test

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,14 @@
 #./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_512_128
 #rm out/file.raw 
 
-./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_128.jpg -t path -s 128 -r 720
-./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_128
+./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_256.jpg -t path -s 256 -r 720
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_256
 rm out/file.raw 
 
-#./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_128.jpg -t path -s 128 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_128
-#rm out/file.raw 
+./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_256.jpg -t path -s 256 -r 720
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_256
+rm out/file.raw 
 
-#./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_128.jpg -t path -s 128 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_128
-#rm out/file.raw 
+./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_256.jpg -t path -s 256 -r 720
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_256
+rm out/file.raw 

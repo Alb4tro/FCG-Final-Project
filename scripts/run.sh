7d58b72ada1ec57c161216f725f8171436bb0ccd
albatrosss <albertomongardini@gmail.com>
2020-05-25 11:19:43 +0200

    new tests added

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,30 @@
 #./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_512_128
 #rm out/file.raw 
 
-./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_256.jpg -t path -s 256 -r 720
-./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_256
+#./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_256.jpg -t path -s 256 -r 720
+#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_256
+#rm out/file.raw 
+
+#./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_256.jpg -t path -s 256 -r 720
+#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_256
+#rm out/file.raw 
+
+#./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_256.jpg -t path -s 256 -r 720
+#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_256
+#rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/01_cornellbox/cornellbox.json -o out/lowres/01_cornellbox_512_256.jpg -t path -s 256 -r 512
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_512_256
 rm out/file.raw 
 
-./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_256.jpg -t path -s 256 -r 720
-./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_256
+./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_512.jpg -t path -s 512 -r 720
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_512
 rm out/file.raw 
 
-./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_256.jpg -t path -s 256 -r 720
-./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_256
+./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_512.jpg -t path -s 512 -r 720
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_512
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_512.jpg -t path -s 512 -r 720
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_512
 rm out/file.raw 
--- scripts/run_nlm.sh
+++ scripts/run_nlm.sh
@@ -1,11 +1,23 @@
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_128.jpg --output out/nlm_denoised_images/denoised_cornellbox_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_128.jpg --output out/nlm_denoised_images/denoised_cornellbox_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_128.jpg --output out/nlm_denoised_images/denoised_cornellbox_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_128.jpg --output out/nlm_denoised_images/denoised_cornellbox_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/11_bathroom_720_128.jpg --output out/nlm_denoised_images/denoised_bathroom_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/11_bathroom_720_128.jpg --output out/nlm_denoised_images/denoised_bathroom_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/11_bathroom_720_256.jpg --output out/nlm_denoised_images/denoised_bathroom_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/11_bathroom_720_256.jpg --output out/nlm_denoised_images/denoised_bathroom_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_128.jpg --output out/nlm_denoised_images/denoised_bedroom_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_128.jpg --output out/nlm_denoised_images/denoised_bedroom_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_256.jpg --output out/nlm_denoised_images/denoised_bedroom_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_256.jpg --output out/nlm_denoised_images/denoised_bedroom_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/17_kitchen_720_128.jpg --output out/nlm_denoised_images/denoised_kitchen_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/17_kitchen_720_128.jpg --output out/nlm_denoised_images/denoised_kitchen_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_kitchen_720_256.jpg --output out/nlm_denoised_images/denoised_kitchen_128_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_kitchen_720_256.jpg --output out/nlm_denoised_images/denoised_kitchen_128_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_256.jpg --output out/nlm_denoised_images/denoised_cornellbox_256_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_256.jpg --output out/nlm_denoised_images/denoised_cornellbox_256_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/11_bathroom_720_512.jpg --output out/nlm_denoised_images/denoised_bathroom_512_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/11_bathroom_720_512.jpg --output out/nlm_denoised_images/denoised_bathroom_512_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_512.jpg --output out/nlm_denoised_images/denoised_bedroom_512_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+#bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_512.jpg --output out/nlm_denoised_images/denoised_bedroom_512_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/17_kitchen_720_512.jpg --output out/nlm_denoised_images/denoised_kitchen_512_1.png --patch_r 1 --big_r 10 --h 1.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/17_kitchen_720_512.jpg --output out/nlm_denoised_images/denoised_kitchen_512_2.png --patch_r 1 --big_r 10 --h 2  --sigma 10

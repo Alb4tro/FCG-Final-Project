29b57244b3c8165b883368ec8a86549de9432410
unknown <albertomongardini@gmail.com>
2020-05-26 19:53:04 +0200

    tests and clean

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,34 +1,3 @@
-#./bin/RelWithDebInfo/yscenetrace tests/01_cornellbox/cornellbox.json -o out/lowres/01_cornellbox_512_128.jpg -t path -s 128 -r 512
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_512_128
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_256.jpg -t path -s 256 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_256
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_256.jpg -t path -s 256 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_256
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_256.jpg -t path -s 256 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_256
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/01_cornellbox/cornellbox.json -o out/lowres/01_cornellbox_512_256.jpg -t path -s 256 -r 512
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_512_256
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_512.jpg -t path -s 512 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_512
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_720_512.jpg -t path -s 512 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_512
-#rm out/file.raw 
-
-#./bin/RelWithDebInfo/yscenetrace tests/17_kitchen/kitchen.json -o out/lowres/17_kitchen_720_512.jpg -t path -s 512 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_kitchen_720_512
-#rm out/file.raw 
 
 #./bin/RelWithDebInfo/yscenetrace tests/11_bathroom1/bathroom1.json -o out/lowres/11_bathroom_720_1024.jpg -t path -s 1024 -r 720
 #./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/11_bathroom_720_1024
@@ -38,6 +7,12 @@
 #./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_720_1024
 #rm out/file.raw
 
-#./bin/RelWithDebInfo/yscenetrace tests/12_ecosys/ecosys.json -o out/lowres/12_ecosys_720_64.jpg -t path -s 64 -r 720
-#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/12_ecosys_720_64
+### DA TENERE ###
+
+#./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_720_128.jpg -t path -s 128 -r 720
+#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_720_64
+#rm out/file.raw 
+
+#./bin/RelWithDebInfo/yscenetrace tests/01_cornellbox/cornellbox.json -o out/lowres/01_cornellbox_512_256.jpg -t path -s 256 -r 512
+#./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_512_256
 #rm out/file.raw 

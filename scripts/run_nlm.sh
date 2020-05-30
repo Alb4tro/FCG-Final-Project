6fc8ccf271ee246311fb7cbb44ae13edb5ea9897
albatrosss <albertomongardini@gmail.com>
2020-05-30 13:50:13 +0200

    added images

--- scripts/run_nlm.sh
+++ scripts/run_nlm.sh
@@ -1,7 +1,18 @@
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_720_1024.jpg --output out/nlm_denoised_images/denoised_bedroom_1024_3.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+# Bedroom
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_720_256.jpg --output out/nlm_denoised_images/denoised_coffee_256_4.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_4.jpg --output out/nlm_denoised_images/denoised_bedroom_4.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_8.jpg --output out/nlm_denoised_images/denoised_bedroom_8.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_16.jpg --output out/nlm_denoised_images/denoised_bedroom_16.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_64.jpg --output out/nlm_denoised_images/denoised_bedroom_64.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_128.jpg --output out/nlm_denoised_images/denoised_bedroom_128.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_256.jpg --output out/nlm_denoised_images/denoised_bedroom_256.png --patch_r 1 --big_r 60 --h 2  --sigma 10
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_512_256.jpg --output out/nlm_denoised_images/denoised_cornellbox_256_8.png --patch_r 2 --big_r 10 --h 3  --sigma 5
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/04_head_720_64.jpg --output out/nlm_denoised_images/denoised_head_720_64.png --patch_r 1 --big_r 10 --h 2  --sigma 10
+# Cornellbox
+
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_4.jpg --output out/nlm_denoised_images/denoised_cornellbox_4.png --patch_r 2 --big_r 10 --h 3  --sigma 5
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_8.jpg --output out/nlm_denoised_images/denoised_cornellbox_8.png --patch_r 2 --big_r 10 --h 3  --sigma 5
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_16.jpg --output out/nlm_denoised_images/denoised_cornellbox_16.png --patch_r 2 --big_r 10 --h 3  --sigma 5
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_64.jpg --output out/nlm_denoised_images/denoised_cornellbox_64.png --patch_r 2 --big_r 10 --h 3  --sigma 5
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_128.jpg --output out/nlm_denoised_images/denoised_cornellbox_128.png --patch_r 2 --big_r 10 --h 3  --sigma 5
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_256.jpg --output out/nlm_denoised_images/denoised_cornellbox_256.png --patch_r 2 --big_r 10 --h 3  --sigma 5

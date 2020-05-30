906f3602363fb7e3efc0e8ee04201eccfa8abfc9
unknown <albertomongardini@gmail.com>
2020-05-30 16:29:42 +0200

    bedroom and cornellbox tests added

--- scripts/run_nlm.sh
+++ scripts/run_nlm.sh
@@ -1,11 +1,12 @@
 # Bedroom
 
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_4.jpg --output out/nlm_denoised_images/denoised_bedroom_4.png --patch_r 1 --big_r 60 --h 2  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_8.jpg --output out/nlm_denoised_images/denoised_bedroom_8.png --patch_r 1 --big_r 60 --h 2  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_16.jpg --output out/nlm_denoised_images/denoised_bedroom_16.png --patch_r 1 --big_r 60 --h 2  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_64.jpg --output out/nlm_denoised_images/denoised_bedroom_64.png --patch_r 1 --big_r 60 --h 2  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_128.jpg --output out/nlm_denoised_images/denoised_bedroom_128.png --patch_r 1 --big_r 60 --h 2  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_256.jpg --output out/nlm_denoised_images/denoised_bedroom_256.png --patch_r 1 --big_r 60 --h 2  --sigma 10
+
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_4.jpg --output out/nlm_denoised_images/denoised_bedroom_4.png --patch_r 2 --big_r 10 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_8.jpg --output out/nlm_denoised_images/denoised_bedroom_8.png --patch_r 2 --big_r 10 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_16.jpg --output out/nlm_denoised_images/denoised_bedroom_16.png --patch_r 2 --big_r 10 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_64.jpg --output out/nlm_denoised_images/denoised_bedroom_64.png --patch_r 2 --big_r 10 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_128.jpg --output out/nlm_denoised_images/denoised_bedroom_128.png --patch_r 2 --big_r 10 --h 2  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_256.jpg --output out/nlm_denoised_images/denoised_bedroom_256.png --patch_r 2 --big_r 10 --h 2  --sigma 10
 
 
 # Cornellbox

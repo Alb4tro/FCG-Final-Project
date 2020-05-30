1642bb88d4e7307ea19a2e31800ba8bab7c7b8c2
Xeldaine <_Calabrone1>
2020-05-30 17:59:09 +0200

    tests

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,4 @@
-./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_256.jpg -t path -s 256 -r 1080
-./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_256
-rm out/file.raw 
 
-
-./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_256.jpg -t path -s 256 -r 1080
-./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_256
-rm out/file.raw
-
-"""
 #Bedroom
 ./bin/RelWithDebInfo/yscenetrace tests/13_bedroom/bedroom.json -o out/lowres/13_bedroom_1080_4.jpg -t path -s 4 -r 1080
 ./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/13_bedroom_1080_4
@@ -58,4 +49,55 @@ rm out/file.raw
 ./bin/RelWithDebInfo/yscenetrace tests/01_cornellbox/cornellbox.json -o out/lowres/01_cornellbox_1080_256.jpg -t path -s 256 -r 1080
 ./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/01_cornellbox_1080_256
 rm out/file.raw 
-"""
+
+# Coffee
+
+./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_4.jpg -t path -s 4 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_4
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_8.jpg -t path -s 8 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_8
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_16.jpg -t path -s 16 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_16
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_64.jpg -t path -s 64 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_64
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_128.jpg -t path -s 128 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_128
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/16_coffee/coffee.json -o out/lowres/16_coffee_1080_256.jpg -t path -s 256 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/16_coffee_1080_256
+rm out/file.raw 
+
+# Head
+
+./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_4.jpg -t path -s 4 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_4
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_8.jpg -t path -s 8 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_8
+rm out/file.raw
+
+./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_16.jpg -t path -s 16 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_16
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_64.jpg -t path -s 64 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_64
+rm out/file.raw
+
+./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_128.jpg -t path -s 128 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_128
+rm out/file.raw 
+
+./bin/RelWithDebInfo/yscenetrace tests/05_head1ss/head1ss.json -o out/lowres/05_head1ss_1080_256.jpg -t path -s 256 -r 1080
+./bin/RelWithDebInfo/yimagedenoise out/file.raw -o out/bcd_denoised_images/05_head_1080_256
+rm out/file.raw
--- scripts/run_nlm.sh
+++ scripts/run_nlm.sh
@@ -1,9 +1,4 @@
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_256.jpg --output out/nlm_denoised_images/denoised_coffee_1080_256.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
-bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_256.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_256.png --patch_r 1 --big_r 10 --h 2  --sigma 10
-
-
 # Bedroom
-
 
 bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_4.jpg --output out/nlm_denoised_images/denoised_bedroom_4.png --patch_r 2 --big_r 10 --h 2  --sigma 10
 bin/RelWithDebInfo/nlm_denoiser --input out/lowres/13_bedroom_1080_8.jpg --output out/nlm_denoised_images/denoised_bedroom_8.png --patch_r 2 --big_r 10 --h 2  --sigma 10
@@ -21,3 +16,22 @@ bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_16.jpg --o
 bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_64.jpg --output out/nlm_denoised_images/denoised_cornellbox_64.png --patch_r 2 --big_r 10 --h 3  --sigma 5
 bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_128.jpg --output out/nlm_denoised_images/denoised_cornellbox_128.png --patch_r 2 --big_r 10 --h 3  --sigma 5
 bin/RelWithDebInfo/nlm_denoiser --input out/lowres/01_cornellbox_1080_256.jpg --output out/nlm_denoised_images/denoised_cornellbox_256.png --patch_r 2 --big_r 10 --h 3  --sigma 5
+
+# Coffee
+
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_4.jpg --output out/nlm_denoised_images/denoised_coffee_1080_4.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_8.jpg --output out/nlm_denoised_images/denoised_coffee_1080_8.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_16.jpg --output out/nlm_denoised_images/denoised_coffee_1080_16.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_64.jpg --output out/nlm_denoised_images/denoised_coffee_1080_64.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_128.jpg --output out/nlm_denoised_images/denoised_coffee_1080_128.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/16_coffee_1080_256.jpg --output out/nlm_denoised_images/denoised_coffee_1080_256.png --patch_r 1 --big_r 10 --h 2.5  --sigma 10
+
+
+# Head
+
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_4.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_4.png --patch_r 2 --big_r 10 --h 2  --sigma 20
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_8.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_8.png --patch_r 2 --big_r 10 --h 2  --sigma 20
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_16.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_16.png --patch_r 2 --big_r 10 --h 2  --sigma 20
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_64.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_64.png --patch_r 2 --big_r 10 --h 2  --sigma 20
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_128.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_128.png --patch_r 2 --big_r 10 --h 2  --sigma 20
+bin/RelWithDebInfo/nlm_denoiser --input out/lowres/05_head1ss_1080_256.jpg --output out/nlm_denoised_images/denoised_head1ss_1080_256.png --patch_r 2 --big_r 10 --h 2  --sigma 20
